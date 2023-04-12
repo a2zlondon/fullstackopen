678870e5c5851cdc48d997de51f933160d6d6ef4
Daniel Molloy <d4nmolloy@gmail.com>
2023-04-12 12:59:10 +0100

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,38 +9,37 @@ const Header = (props) => {
 }
 
 const Statistics = (props) => {
-  
+
   const all = props.good + props.neutral + props.bad
-  //const average = [good, neutral, bad].reduce((x,y) => x+y)/[good, neutral, bad].length
-  //const average = good/all + (bad/all)*-1
   const average = props.good/all - props.bad/all
-  const positive = (props.good/all)*100
+  const positive = (props.good / all) * 100
+  const anygtzero = (props.good>0) || (props.neutral>0) || (props.bad>0)
 
   return (
-      <div>
-        <h1>
-        statistics
-        </h1>
-        <table>
-          <tr>
-            <td>good</td><td>{props.good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td><td>{props.neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td><td>{props.bad}</td>
-          </tr>
-          <tr>
-            <td>all</td><td>{all}</td>
-          </tr>
-          <tr>
-            <td>average</td><td>{isNaN(average) ? 0 : average }</td>
-          </tr>
-          <tr>
-            <td>positive</td><td>{isNaN(positive) ? 0 : positive }%</td>
-          </tr>
-        </table>
+    <div>
+      {anygtzero && (          <table>
+          <tbody>
+              <tr>
+                <td>good</td><td>{props.good}</td>
+              </tr>
+              <tr>
+                <td>neutral</td><td>{props.neutral}</td>
+              </tr>
+              <tr>
+                <td>bad</td><td>{props.bad}</td>
+              </tr>
+              <tr>
+                <td>all</td><td>{all}</td>
+              </tr>
+              <tr>
+                <td>average</td><td>{isNaN(average) ? 0 : average }</td>
+              </tr>
+              <tr>
+                <td>positive</td><td>{isNaN(positive) ? 0 : positive }%</td>
+              </tr>
+          </tbody>
+      </table>)}
+      <p>No feedback given</p>
       </div>
     )
 }
@@ -61,6 +60,10 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h1>
+        statistics
+      </h1>
 
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
