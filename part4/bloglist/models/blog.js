4f3aff36e233acaa4077db428f9eee7aa1a5a43d
Daniel Molloy <d4nmolloy@gmail.com>
2023-05-12 12:29:15 +0100

    Exercise 4.1 create the backend for the blog list

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,48 @@
+// const mongoose = require('mongoose')
+
+// mongoose.set('strictQuery', false)
+
+// const url = process.env.MONGODB_URI
+
+// console.log('connecting to', url)
+
+// mongoose.connect(url)
+//   .then((result) => {
+//     console.log(`connected to MongoDB ${result}`)
+//   })
+//   .catch((error) => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
+
+// const blogSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     minLength: 3,
+//     required: true
+//   },
+//   number: {
+//     type: String,
+//     minLength: 8,
+//     validate: {
+//       validator: function (enteredstring) {
+//         if (enteredstring.split('-').length === 2) {
+//           return /\d{2,3}-\d+/.test(enteredstring)
+//         } else {
+//           return false
+//         }
+//       },
+//       message: props => `${props.value} is not a valid phone number!`
+//     },
+//     required: [true, 'User phone number required']
+//   }
+// })
+
+// personSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// module.exports = mongoose.model('Person', personSchema)
---
+++ part4/bloglist/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const title = process.argv[3]
+const author = process.argv[4]
+const url = process.argv[5]
+const likes = process.argv[6]
+
+const mongourl = `mongodb+srv://fullstackopen:${password}@fullstackopen-phonebook.jyrvi41.mongodb.net/bloglistApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(mongourl)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+if (!title && !author) {
+
+  Blog.find({}).then(result => {
+    result.forEach(p => {
+      console.log(`${p.title} ${p.author}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+  })
+  blog.save().then(result => {
+    console.log(`added ${result.title} number ${result.author} to bloglist`)
+    mongoose.connection.close()
+  })
+}
